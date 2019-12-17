6a8fe425aa7214e61fd98808266b93b6b75cdca7
Delpen9 <iantdover@gmail.com>
2019-12-16 20:44:26 -0600

    Step-Up Function within Create Wav File From Bits

--- create_dynamic_wav_file_from_bits.py
+++ create_dynamic_wav_file_from_bits.py
@@ -18,11 +18,11 @@ for i in range(1, 11):
   y = np.append(y, 10*np.sin(frequency * 2 * np.pi * t))
         
 for i in range(11, 290):
-  # 10600 = 1; 10800 = 2; 11000 = 3; 11200 = 4; ... ; 12000 = 8; 12200 = 9; 12400 = 10
-  frequency = 10400 + 200*np.abs(10 - i) % 10
+  frequency = 10600 + 200*(np.abs(10 - i) % 10)
   t = np.linspace(duration + move_ahead, 2 * duration, sampleRate * duration)
   y = np.append(y, 10*np.sin(frequency * 2 * np.pi * t))
-  
+
+frequency = 12600
 for i in range(290, 300):
   t = np.linspace(duration + move_ahead, 2 * duration, sampleRate * duration)
   y = np.append(y, 10*np.sin(frequency * 2 * np.pi * t))
